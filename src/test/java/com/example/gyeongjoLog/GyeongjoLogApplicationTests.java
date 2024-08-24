bc830e7a0d8d274c16363afbf95d926ed2ac1c6b
pjs9797 <1997pjs@naver.com>
2024-08-24 17:09:18 +0900

    initial

---
+++ src/test/java/com/example/gyeongjoLog/GyeongjoLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gyeongjoLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyeongjoLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
